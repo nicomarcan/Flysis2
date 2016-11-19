c3167726a1c8c60485ed38902624f9fd700415cd
traies <traies@itba.edu.ar>
2016-11-19 02:33:00 -0300

    update flight_info_activity.xml

--- app/src/main/java/com/example/nmarcantonio/flysys2/FlightActivity.java
+++ app/src/main/java/com/example/nmarcantonio/flysys2/FlightActivity.java
@@ -3,8 +3,10 @@ package com.example.nmarcantonio.flysys2;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -25,16 +27,25 @@ public class FlightActivity extends AppCompatActivity implements OnMapReadyCallb
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
         setContentView(R.layout.flight_info_activity);
+
         /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         );
-
+        */
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        CollapsingToolbarLayout ctl = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        ctl.setTitle("Vuelo XXXX");
+        ctl.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
         /*
         ScrollView scrollView = (ScrollView) findViewById(R.id.flight_info_scroll);
 
